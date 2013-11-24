dc014ff13a78a1770f0cfb3c2c1169524f830134
Jonas Eschenburg <jonas@tnlgame.net>
2013-11-24 16:45:55 +0100

    Modified env-vars.sh to work on linux

--- env-vars.sh
+++ env-vars.sh
@@ -1,3 +1,4 @@
 export GOPATH=$(cd gopath; pwd)
-export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
+
+command -v brew >/dev/null 2>&1 && export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
 
