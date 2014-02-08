96b24f07d29905e7d1bd2134f16e506c7a80d9ee
Jonas Eschenburg <jonas@bitwrk.net>
2014-02-09 00:01:51 +0100

    Infrastructure: Change project structure so compiling is easier and Goclipse can be used (fixes #44)

--- env-vars.sh
+++
@@ -1,4 +0,0 @@
-export GOPATH=$(cd gopath; pwd)
-
-command -v brew >/dev/null 2>&1 && export PYTHONPATH=$(brew --prefix)/lib/python2.7/site-packages
-
--- simulate-buy.sh
+++
@@ -1,3 +0,0 @@
-#/bin/sh
-curl -v -L -F data=@GPLV3.txt http://localhost:8081/buy/foobar
-
