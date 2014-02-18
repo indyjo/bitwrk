indyjo/bitwrk

commit 09a58c65ede63d5e299ac4dee511e2e708eedcd2

Close connection to seller (in buys) and connection to worker (in sells) when transaction times out (fixes #40, fixes #35, fixes #32) React to closed connection in blender-slave.py

bitwrk-blender/blender-slave.py
@@ -24,6 +24,7 @@ if sys.version_info[:2] < (3,2):
     raise RuntimeError("Python >= 3.2 required. Detected: %s" % sys.version_info)
 
 import http.server, urllib.request, urllib.parse, struct, os, tempfile, subprocess
+from select import select
 
 # decode http chunked encoding
 class Unchunked:
@@ -218,7 +219,21 @@ class BlenderHandler(http.server.BaseHTTPRequestHandler):
                 '--render-frame', '%d' % frame,
                 ]
             print("Calling", args)
-            subprocess.check_call(args)
+            #subprocess.check_call(args)
+            with subprocess.Popen(args, stdin=subprocess.DEVNULL) as proc:
+                while True:
+                    retcode = proc.poll()
+                    if retcode == 0:
+                        break
+                    elif retcode is not None:
+                        self.send_response(500)
+                        return
+                    rl, _, _ = select([self.rfile], [], [], 100)
+                    if self.rfile in rl:
+                        print("ERROR request cancelled")
+                        proc.kill()
+                        return
+                
             #subprocess.check_call(['/bin/sleep','120'])
             
             self.send_response(200)

bitwrk-client/client_main.go
@@ -163,8 +163,8 @@ func serveInternal(workerManager *client.WorkerManager, exit chan<- error) {
 	s := &http.Server{
 		Addr:         fmt.Sprintf("localhost:%v", InternalPort),
 		Handler:      mux,
-		ReadTimeout:  300 * time.Second,
-		WriteTimeout: 300 * time.Second,
+		ReadTimeout:  900 * time.Second,
+		WriteTimeout: 900 * time.Second,
 	}
 	relay := &HttpRelay{"/", client.BitwrkUrl}
 	mux.Handle("/account/", relay)
@@ -211,8 +211,8 @@ func serveExternal(receiveManager *client.ReceiveManager, exit chan<- error) {
 	s := &http.Server{
 		Addr:         fmt.Sprintf(":%v", ExternalPort),
 		Handler:      mux,
-		ReadTimeout:  300 * time.Second,
-		WriteTimeout: 300 * time.Second,
+		ReadTimeout:  900 * time.Second,
+		WriteTimeout: 900 * time.Second,
 	}
 
 	mux.Handle("/", receiveManager)
@@ -274,6 +274,8 @@ func handleBuy(w http.ResponseWriter, r *http.Request) {
 	}
 	defer buy.End()
 
+	log := bitwrk.Root().Newf("Buy #%v", buy.GetKey())
+
 	workWriter := buy.WorkWriter()
 
 	var reader io.Reader
@@ -307,7 +309,7 @@ func handleBuy(w http.ResponseWriter, r *http.Request) {
 	}
 
 	var result cafs.File
-	if res, err := buy.PerformBuy(bitwrk.Root().Newf("Buy #%v", buy.GetKey())); err != nil {
+	if res, err := buy.PerformBuy(log); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		log.Printf("Error receiving result from BitWrk network: %v", err)
 		return

gopath/src/bitwrk/client/buy.go
@@ -26,6 +26,7 @@ import (
 	"fmt"
 	"io"
 	"mime/multipart"
+	"net"
 	"net/http"
 	"net/url"
 	"sync"
@@ -185,6 +186,8 @@ func (a *BuyActivity) transmitWorkAndReceiveEncryptedResult(log bitwrk.Logger) e
 	// Send work to client
 	pipeIn, pipeOut := io.Pipe()
 	mwriter := multipart.NewWriter(pipeOut)
+
+	// Write work file into pipe for HTTP request
 	go func() {
 		part, err := mwriter.CreateFormFile("work", "workfile.bin")
 		if err != nil {
@@ -192,7 +195,7 @@ func (a *BuyActivity) transmitWorkAndReceiveEncryptedResult(log bitwrk.Logger) e
 			return
 		}
 		work := a.workFile.Open()
-		log.Printf("Sending work data to seller [%v].", a.tx.WorkerURL)
+		log.Printf("Sending work data to seller [%v].", *a.tx.WorkerURL)
 		_, err = io.Copy(part, work)
 		work.Close()
 		if err != nil {
@@ -223,8 +226,26 @@ func (a *BuyActivity) transmitWorkAndReceiveEncryptedResult(log bitwrk.Logger) e
 		return fmt.Errorf("Error creating transmit request: %v", err)
 	} else {
 		req.Header.Set("Content-Type", mwriter.FormDataContentType())
-		if resp, err := client.Do(req); err != nil {
-			return fmt.Errorf("Error fetching request %v: %v", req, err)
+
+		var controlledClient http.Client = client
+		controlledClient.Transport = &http.Transport{
+			Dial: func(network, addr string) (conn net.Conn, err error) {
+				conn, err = net.DialTimeout(network, addr, 10*time.Second)
+				if err == nil {
+					// Keep watching tx and close connection when retired before end of transmission
+					go func() {
+						if err := a.awaitTransactionPhase(log, bitwrk.PhaseUnverified, bitwrk.PhaseWorking, bitwrk.PhaseTransmitting); err != nil {
+							log.Printf("Closing connection to seller: %v", err)
+							conn.Close()
+						}
+					}()
+				}
+				return
+			},
+		}
+
+		if resp, err := controlledClient.Do(req); err != nil {
+			return err
 		} else {
 			response = resp.Body
 		}

gopath/src/bitwrk/client/protocol.go
@@ -25,16 +25,24 @@ import (
 	"fmt"
 	"io"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"net/url"
 	"sort"
 	"strings"
+	"time"
 )
 
-// Disallow redirects (or explicitly handle them)
 var client = http.Client{
+	// Disallow redirects (or explicitly handle them)
 	CheckRedirect: func(r *http.Request, _ []*http.Request) error {
 		return fmt.Errorf("Redirect encountered in request %v", r)
+	},
+	// 10 seconds timeout for TCP connection establishing
+	Transport: &http.Transport{
+		Dial: func(network, addr string) (net.Conn, error) {
+			return net.DialTimeout(network, addr, 10*time.Second)
+		},
 	},
 }
 

gopath/src/bitwrk/client/sell.go
@@ -29,6 +29,7 @@ import (
 	"fmt"
 	"io"
 	"mime/multipart"
+	"net"
 	"net/http"
 	"sync"
 	"time"
@@ -96,15 +97,19 @@ func (a *SellActivity) PerformSell(log bitwrk.Logger, receiveManager *ReceiveMan
 	} else {
 		a.tx = tx
 		a.txETag = etag
-		log.Printf("Tx-etag: %#v", etag)
+		//log.Printf("Tx-etag: %#v", etag)
 	}
 
 	// TODO: Verify the transaction
 
 	// Start polling for state changes in background
+	abortPolling := make(chan bool)
+	defer func() {
+		// Stop polling when sell has ended
+		abortPolling <- true
+	}()
 	go func() {
-		a.pollTransaction(log)
-		log.Printf("Transaction polling has stopped.")
+		a.pollTransaction(log, abortPolling)
 	}()
 
 	var backchannel *backchannel
@@ -120,15 +125,15 @@ func (a *SellActivity) PerformSell(log bitwrk.Logger, receiveManager *ReceiveMan
 		return fmt.Errorf("Error publishing buyer's secret: %v", err)
 	}
 
-	log.Println("Awaiting receipt...")
-	endpoint.SetHandler(func(w http.ResponseWriter, r *http.Request) {
-		a.handleReceipt(log.New("handleReceipt"), w, r)
-	})
-
-	if err := a.dispatchWorkAndSaveEncryptedResult(backchannel.workFile); err != nil {
+	log.Println("Dispatching work to", a.workerInfo)
+	if err := a.dispatchWorkAndSaveEncryptedResult(
+		log.New("dispatchWorkAndSaveEncryptedResult"),
+		backchannel.workFile); err != nil {
 		backchannel.release <- 0
 		return fmt.Errorf("Error dispatching work and saving encrypted result: %v", err)
 	}
+
+	log.Println("Checking transaction state")
 
 	// Getting the result has possibly taken too long
 	a.condition.L.Lock()
@@ -140,10 +145,15 @@ func (a *SellActivity) PerformSell(log bitwrk.Logger, receiveManager *ReceiveMan
 	}
 
 	log.Println("Transmitting encrypted result back to buyer")
-	if err := a.transmitEncryptedResultBackToBuyer(backchannel.w); err != nil {
+	if err := a.transmitEncryptedResultBackToBuyer(log.New("send result"), backchannel.w); err != nil {
 		backchannel.release <- 0
 		return fmt.Errorf("Error transmitting encrypted result back to buyer: %v", err)
 	}
+
+	log.Println("Waiting for receipt...")
+	endpoint.SetHandler(func(w http.ResponseWriter, r *http.Request) {
+		a.handleReceipt(log.New("handleReceipt"), w, r)
+	})
 
 	backchannel.release <- 0
 
@@ -151,11 +161,30 @@ func (a *SellActivity) PerformSell(log bitwrk.Logger, receiveManager *ReceiveMan
 	return nil
 }
 
-func (a *SellActivity) dispatchWorkAndSaveEncryptedResult(workFile cafs.File) error {
+func (a *SellActivity) dispatchWorkAndSaveEncryptedResult(log bitwrk.Logger, workFile cafs.File) error {
+	// Watch transaction state and close connection to worker when transaction expires
+	connChan := make(chan io.Closer)
+	exitChan := make(chan bool)
+	go a.watchdog(log, exitChan, connChan, func() bool { return a.tx.State == bitwrk.StateActive })
+	defer func() {
+		exitChan <- true
+	}()
+
+	// Customized HTTP client that submits all connections to watchdog
+	var controlledClient http.Client = client
+	controlledClient.Transport = &http.Transport{
+		Dial: func(network, addr string) (conn net.Conn, err error) {
+			conn, err = net.DialTimeout(network, addr, 10*time.Second)
+			if err == nil {
+				connChan <- conn // Start watching connection
+			}
+			return
+		},
+	}
+
 	reader := workFile.Open()
 	defer reader.Close()
-
-	resp, err := client.Post(a.workerInfo.PushURL, "application/octet-stream", reader)
+	resp, err := controlledClient.Post(a.workerInfo.PushURL, "application/octet-stream", reader)
 	if err != nil {
 		return err
 	}
@@ -200,7 +229,33 @@ func (a *SellActivity) dispatchWorkAndSaveEncryptedResult(workFile cafs.File) er
 	return nil
 }
 
-func (a *SellActivity) transmitEncryptedResultBackToBuyer(writer io.Writer) error {
+// An io.Closer implementation using the http Hijacker feature
+type hijackCloser struct {
+	target interface{}
+}
+
+func (w hijackCloser) Close() error {
+	if hijacker, ok := w.target.(http.Hijacker); ok {
+		if conn, _, err := hijacker.Hijack(); err != nil {
+			return err
+		} else {
+			return conn.Close()
+		}
+	}
+	return nil
+}
+
+func (a *SellActivity) transmitEncryptedResultBackToBuyer(log bitwrk.Logger, writer io.Writer) error {
+	exitChan := make(chan bool)
+	closerChan := make(chan io.Closer)
+	go a.watchdog(log, exitChan, closerChan, func() bool { return a.tx.State == bitwrk.StateActive })
+	defer func() {
+		exitChan <- true
+	}()
+
+	// Close HTTP connection when transaction expires
+	closerChan <- hijackCloser{writer}
+
 	reader := a.encResultFile.Open()
 	defer reader.Close()
 
@@ -223,10 +278,10 @@ type backchannel struct {
 func (a *SellActivity) receiveWorkData(log bitwrk.Logger, endpoint *Endpoint) (*backchannel, error) {
 	result := backchannel{
 		ready:   false,
-		release: make(chan int),
+		release: make(chan int, 1),
 	}
 	handler := func(w http.ResponseWriter, r *http.Request) {
-		a.handleRequest(log, w, r, &result)
+		a.handleBuyerRequest(log, w, r, &result)
 	}
 
 	endpoint.SetHandler(handler)
@@ -259,7 +314,7 @@ func (a *SellActivity) receiveWorkData(log bitwrk.Logger, endpoint *Endpoint) (*
 // Handles the incoming work request, up to the point where the work package
 // has been identified as legit. Then, the controlling goroutine takes over
 // while the request handler waits for the back-channel to be released.
-func (a *SellActivity) handleRequest(log bitwrk.Logger, w http.ResponseWriter, r *http.Request, backchannel *backchannel) {
+func (a *SellActivity) handleBuyerRequest(log bitwrk.Logger, w http.ResponseWriter, r *http.Request, backchannel *backchannel) {
 	if r.Method != "POST" {
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 		return

gopath/src/bitwrk/client/trade.go
@@ -22,6 +22,7 @@ import (
 	"bitwrk/cafs"
 	"bitwrk/money"
 	"fmt"
+	"io"
 	"sync"
 	"time"
 )
@@ -200,14 +201,57 @@ func (t *Trade) waitWhile(f func() bool) {
 	}
 }
 
-// Polls the transaction state in a separate go-routine
-func (t *Trade) pollTransaction(log bitwrk.Logger) {
+// Closes resources upon exit of a function or when some condition no longer holds
+// Arguments:
+//  - exitChan: Signals the watchdog to exit
+//  - closerChan: Signals the watchdog to add an io.Closer to the list of closers
+//  - f: Defines the OK condition. When false, all current closers are closed
+func (t *Trade) watchdog(log bitwrk.Logger, exitChan <-chan bool, closerChan <-chan io.Closer, f func() bool) {
+	closers := make([]io.Closer, 0, 1)
+	for {
+		select {
+		case closer := <-closerChan:
+			closers = append(closers, closer)
+		case <-exitChan:
+			// Exit from watchdog if surrounding function has terminated
+			log.Print("Watchdog exiting by request")
+			return
+		default:
+		}
+
+		// Check if condition still holds
+		t.condition.L.Lock()
+		ok := f()
+		t.condition.L.Unlock()
+		if !ok {
+			log.Printf("Watchdog: closing %v channels", len(closers))
+			for _, c := range closers {
+				err := c.Close()
+			}
+			closers = closers[:0] // clear list of current closers
+		}
+
+		time.Sleep(250 * time.Millisecond)
+	}
+}
+
+// Polls the transaction state in a separate go-routine. Returns on abort signal, or
+// when the polled transaction expires.
+func (t *Trade) pollTransaction(log bitwrk.Logger, abort <-chan bool) {
+	defer func() {
+		log.Printf("Transaction polling has stopped")
+	}()
 
 	for count := 1; ; count++ {
+		select {
+		case <-abort:
+			log.Printf("Aborting transaction polling while transaction active")
+			return
+		default:
+		}
 
 		if tx, etag, err := FetchTx(t.txId, ""); err != nil {
 			log.Printf("Error polling transaction: %v", err)
-			return
 		} else if etag != t.txETag {
 			t.condition.L.Lock()
 			t.tx = tx
@@ -215,10 +259,9 @@ func (t *Trade) pollTransaction(log bitwrk.Logger) {
 			expired := t.tx.State != bitwrk.StateActive
 			t.condition.Broadcast()
 			t.condition.L.Unlock()
-			log.Printf("Tx-etag: %#v", etag)
+			log.Printf("Tx change detected: phase=%v, expired=%v", t.tx.Phase, expired)
 			if expired {
-				log.Printf(" --> expired")
-				return
+				break
 			}
 		}
 
@@ -226,6 +269,9 @@ func (t *Trade) pollTransaction(log bitwrk.Logger) {
 		time.Sleep(time.Duration(count) * 500 * time.Millisecond)
 	}
 
+	log.Printf("Transaction has expired.")
+	// This is necessary so that the surrounding function call doesn't deadlock
+	<-abort
 }
 
 // Implement Activity

commit fc8f64b0b3b9be3400b0561cf4623ca8a9204d46

Fix performance regression introduced in 09a58c65ede63d5e299ac4dee511e2e708eedcd2. It took up to 100 seconds to return resulting Blender data to bitwrk client after Blender had quit.

bitwrk-blender/blender-slave.py
@@ -230,7 +230,7 @@ class BlenderHandler(http.server.BaseHTTPRequestHandler):
                     elif retcode is not None:
                         self.send_response(500)
                         return
-                    rl, _, _ = select([self.rfile], [], [], 100)
+                    rl, _, _ = select([self.rfile], [], [], timeout=1.0)
                     if self.rfile in rl:
                         print("ERROR request cancelled")
                         proc.kill()