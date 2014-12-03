indyjo/bitwrk

commit a3f5fc0127e9c0b89062bd92569b1fb523d3ea9d

UI design for the account page (#96)

share/bitwrk-client/htroot/js/motd.js
@@ -4,7 +4,8 @@ function newMessageUpdater(alertbox, content) {
 	alertbox.bind('closed.bs.alert', function() {
 		lastval = currentval;
 		$.cookie("lastmotd", currentval, {
-			'expires' : 1
+			'expires' : 1,
+			'path' : '/',
 		});
 	});
 	return function() {
@@ -13,9 +14,9 @@ function newMessageUpdater(alertbox, content) {
 			if (xhr.readyState === 4 && xhr.status == 200) {
 				currentval = xhr.responseText;
 				if (currentval != lastval) {
-					var motd=JSON.parse(currentval)
+					var motd = JSON.parse(currentval)
 					content.html(motd.Text);
-					
+
 					alertbox.removeClass("alert-info");
 					alertbox.removeClass("alert-warning");
 					alertbox.removeClass("alert-danger");

share/bitwrk-client/htroot/css/client.css
@@ -1,17 +1,19 @@
 /* Color style: http://colorschemedesigner.com/#3951YckGrp9Iu */
-
 body {
-	padding-top: 100px;
+	padding-top: 120px;
 }
 
 header {
-    color: #999;
+	color: #999;
 	background-color: #444;
 	position: fixed;
 	top: 0;
 	width: 100%;
 	z-index: 1;
 	padding: 0px 0px 1px 0px;
+	-webkit-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.47);
+	-moz-box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.47);
+	box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.47);
 }
 
 header nav ul {
@@ -64,27 +66,27 @@ header div.logo img {
 header div.alert {
 	margin: 1px 0px 0px 0px;
 	border-radius: 0;
-} 
+}
 
 #account-info div {
-    display: inline;
+	display: inline;
 }
 
 #account-info .available, #account-info .participant a {
-    color: #79b4e1;
-    cursor: pointer;
-    font-weight: lighter;
+	color: #79b4e1;
+	cursor: pointer;
+	font-weight: lighter;
 }
 
 #account-info .blocked, #account-info .available {
-    float: right;
+	float: right;
 }
 
 #account-info div.participant {
-    font-size: 75%;
-    vertical-align: middle;
-    display: inline-block;
-    line-height: 13px;
+	font-size: 75%;
+	vertical-align: middle;
+	display: inline-block;
+	line-height: 13px;
 	padding-left: 6px;
 	padding-right: 6px;
 }
@@ -107,21 +109,22 @@ header div.alert {
 }
 
 div.activity, div.worker, div.mandate {
-    border: 1px solid #777;
-    border-radius: .3em;
-    margin-bottom: 4px;
-    box-shadow: inset 0 2px 8px -7px #000;
-    padding: .4em;
-    overflow: hidden;
+	border: 1px solid #777;
+	border-radius: .3em;
+	margin-bottom: 4px;
+	box-shadow: inset 0 2px 8px -7px #000;
+	padding: .4em;
+	overflow: hidden;
 }
 
-.activity .info, .mandate .validuntil, .mandate .tradesleft, .lasterror {
-    font-size: 75%;
-    margin-left: 1em;
+.activity .info, .mandate .validuntil, .mandate .tradesleft, .lasterror
+	{
+	font-size: 75%;
+	margin-left: 1em;
 }
 
 div.activity {
-    background-color: #d1dcef;
+	background-color: #d1dcef;
 }
 
 div.activity.history {
@@ -129,39 +132,40 @@ div.activity.history {
 }
 
 div.worker {
-    background-color: #ceefe6;
+	background-color: #ceefe6;
 }
 
 div.mandate {
-    background-color: #e6fad7;
+	background-color: #e6fad7;
 }
 
-.activity .type, .mandate .type, .key, .tradesleft, .validuntil, .lasterror {
-    float: left;
+.activity .type, .mandate .type, .key, .tradesleft, .validuntil,
+	.lasterror {
+	float: left;
 }
 
 .article {
-    text-align: center;
-    text-shadow: 0px 1px 2px #fff;
-    margin-left: 1em;
-    margin-right: 1em;
+	text-align: center;
+	text-shadow: 0px 1px 2px #fff;
+	margin-left: 1em;
+	margin-right: 1em;
 }
 
 .activity .price, .mandate .price, .worker .status {
-    float: right;
+	float: right;
 }
 
 .activity .accepted {
-    float: left;
-    margin-right: 0.4em;
+	float: left;
+	margin-right: 0.4em;
 }
 
 .closebtn {
-    float: right;
+	float: right;
 }
 
 .mandate .closebtn, .worker .closebtn {
-    margin-left: 1em;
+	margin-left: 1em;
 }
 
 .lasterror {

share/bitwrk-client/templates/index.html
@@ -86,7 +86,6 @@ $(document).ready(function() {
 <div class="col-sm-4"><h3>Activities</h3><div id="activities"></div></div>
 <div class="col-sm-4"><h3>Workers</h3><div id="workers"></div></div>
 <div class="col-sm-4"><h3>Mandates</h3><div id="mandates"></div></div>
-{{end}}
 <div id="mandateModal" class="modal fade">
 <div class="modal-dialog">
 <div class="modal-content">
@@ -131,6 +130,62 @@ The BitWrk client is asking for your permission to perform a trade.</div>
 </div><!-- modal-body -->
 </div><!-- modal-dialog -->
 </div><!-- modal -->
+{{else if .Page | eq "account"}}
+<div class="col-md-8">
+<div class="panel panel-default">
+<div class="panel-heading">Deposits</div>
+<div class="panel-body">
+<div class="alert alert-warning" role="alert">This functionality hasn't been implemented yet.</div>
+<p class="lead">In order to buy on BitWrk, you need to deposit some amount of <a href="http://bitcoin.org">Bitcoin</a>.</p>
+<p class="help-block">Deposits must be BTC 0.001 minimum. Smaller deposits will be ignored.</p>
+<p>There is currently no Bitcoin deposit address associated with your account.
+<p><a href="?action=requestdepositaddress" class="btn btn-default">Request a new deposit address</a>
+</div>
+</div>
+<div class="panel panel-default">
+<div class="panel-heading">Withdrawals</div>
+<div class="panel-body">
+<div class="alert alert-warning" role="alert">This functionality hasn't been implemented yet.</div>
+<p class="lead">By withdrawing from BitWrk, you can transfer money into your private Bitcoin wallet.</p>
+<p class="help-block">
+You may withdraw Bitcoin once every 24 hours.
+Withdrawals must be BTC 0.001 minimum.
+Processing the transaction may take a couple of hours.</p>
+<form role="form" class="form-horizontal">
+<input type="hidden" name="action" value="requestwithdrawal" />
+<div class="form-group">
+<label for="withdrawalAmount" class="col-md-3 control-label">Withdrawal amount:</label>
+<div class="col-md-9">
+<input id="withdrawalAmount" type="text" name="amount" class="form-control" value="BTC 0.01"/>
+</div>
+</div>
+<div class="form-group">
+<label for="withdrawalDestinationAddress" class="col-md-3 control-label">Destination Address:</label>
+<div class="col-md-9">
+<input id="withdrawalDestinationAddress" type="text" name="destaddress" class="form-control" value="{{.ParticipantId}}"/>
+</div>
+</div>
+<div class="form-group">
+<div class="col-md-offset-3 col-md-9">
+<input class="btn btn-default" type="submit" value="Withdraw to this address">
+</div>
+</div>
+</form>
+</div>
+</div>
+</div>
+<div class="col-md-4">
+<div class="panel panel-default">
+<div class="panel-heading">General Information</div>
+<div class="panel-body">
+<p>You are accessing account <a href="#" onclick="showAccountInfo()">{{.ParticipantId}}</a>
+on BitWrk service <em>{{.ServerURL}}</em>.</p>
+<p>BitWrk is currently in development. Some functionality is not implemented yet.</p>
+<p>Please see <a href="http://bitwrk.net/">bitwrk.net</a> for further information.</p>
+</div>
+</div>
+</div>
+{{end}}
 <div id="iframeModal" class="modal fade">
 <div class="modal-dialog">
 <div class="modal-content">

commit 133bf566b84aa1ed1b55739e8224bc704e0ef117

Message of the day kept popping up again (caused by #96)

share/bitwrk-client/htroot/js/motd.js
@@ -12,7 +12,7 @@ function showAlertBox(alertbox, alertcontent, alertclass, htmlcontent) {
 function newMessageUpdater(alertbox, content) {
 	var lastval = $.cookie("lastmotd");
 	var currentval = "";
-	alertbox.bind('closed.bs.alert', function() {
+	alertbox.find('button.close').on('click', function() {
 		lastval = currentval;
 		$.cookie("lastmotd", currentval, {
 			'expires' : 1,