e90d36c6169459c256c5f2c08056cd45b6e01169
Jonas Eschenburg <jonas@bitwrk.net>
2014-12-05 19:13:57 +0100

    Render deposit address as QR-Code. (#96)

--- share/bitwrk-client/htroot/js/accountinfo.js
+++ share/bitwrk-client/htroot/js/accountinfo.js
@@ -22,35 +22,39 @@ function updateDepositInfo() {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function() {
 		if (xhr.readyState !== 4) {
-			return
-
+			return;
 		}
 
-		$(".depositaddress-yes").addClass("hidden")
-		$(".depositaddress-no").addClass("hidden")
-		$(".depositaddressrequest-yes").addClass("hidden")
-		$(".depositaddressrequest-no").addClass("hidden")
+		$(".depositaddress-yes").addClass("hidden");
+		$(".depositaddress-no").addClass("hidden");
+		$(".depositaddressrequest-yes").addClass("hidden");
+		$(".depositaddressrequest-no").addClass("hidden");
 		if (xhr.status == 200) {
 			var myaccount = JSON.parse(xhr.responseText);
 
 			// Update state of deposit address display
 			if (myaccount.DepositAddressValid) {
-				$(".depositaddress-yes").removeClass("hidden")
-				$(".depositaddress").html(myaccount.DepositAddress)
-				$("a.depositaddress").attr("href",
-						"bitcoin:" + myaccount.DepositAddress)
+				$(".depositaddress-yes").removeClass("hidden");
+				$(".depositaddress").html(myaccount.DepositAddress);
+				var btcURL = "bitcoin:" + myaccount.DepositAddress;
+				$("a.depositaddress").attr("href", btcURL);
+				// Generate a QR code for the address
+				var qrElems = $(".depositaddress-qr").empty().get();
+				for (var i = 0; i < qrElems.length; i++) {
+					new QRCode(qrElems[i], btcURL);
+				}
 			} else {
-				$(".depositaddress-no").removeClass("hidden")
+				$(".depositaddress-no").removeClass("hidden");
 			}
 
 			// Update state of deposit address request display
 			if (myaccount.Account.DepositAddressRequest) {
-				$(".depositaddressrequest-yes").removeClass("hidden")
+				$(".depositaddressrequest-yes").removeClass("hidden");
 			} else {
-				$(".depositaddressrequest-no").removeClass("hidden")
+				$(".depositaddressrequest-no").removeClass("hidden");
 			}
 
-			$(".trustedaccount").html(myaccount.TrustedAccount)
+			$(".trustedaccount").html(myaccount.TrustedAccount);
 		}
 	};
 	xhr.open("GET", "/myaccount");
